65640203f41943a9625c803a1e316bd2a420e2ef
Armin Zahirovic <ani_zahirovic@hotmail.com>
2022-11-20 21:19:36 +0100

    add app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,40 @@
-import { TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { expect } from '@jest/globals';
 
 import { AppComponent } from './app.component';
+import { GameModule } from './game-feature/game.module';
+import { RootStoreModule } from './root-store';
 
-xdescribe('AppComponent', () => {
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let el: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      imports: [
+        GameModule,
+        RootStoreModule
+      ]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should display app-game component', () => {
+    const gameComponent = el.queryAll(By.css(".game-component"));
+    expect(gameComponent).toBeTruthy();
   });
 });
